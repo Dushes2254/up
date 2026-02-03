6007c049020a5879efdd05d94a1f05662ccbf8d0
Andrey Klimenko <79120692680@yandex.ru>
2026-02-03 17:13:45 +0500

    add errorBoundary

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import { Button } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError: FC<PageErrorProps> = ({ className }) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className={classNames(cls.pageError, {}, [className])}>
+      <p>{t('UnexpectedError')}</p>
+      <Button onClick={reloadPage}>{t('RefreshPage')}</Button>
+    </div>
+  );
+};
