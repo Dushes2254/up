fd2724c4028fd332039a31387e8863b6487c138a
Andrey Klimenko <79120692680@yandex.ru>
2026-02-17 15:53:06 +0500

    Add GitHub Actions workflow for linting, testing, and building project; refactor buildPlugins for conditional HotModuleReplacementPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,19 +7,27 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 import { BuildOptions } from './types/config';
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => [
-  new ProgressPlugin(),
-  new HTMLWebpackPlugin({ template: paths.html }),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash].css',
-    chunkFilename: 'css/[name].[contenthash].css',
-  }),
-  new DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev),
-  }),
-  new HotModuleReplacementPlugin(),
-  new BundleAnalyzerPlugin({
-    analyzerMode: isDev ? 'server' : 'disabled',
-    openAnalyzer: false,
-  }),
-];
+export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
+  const plugins = [
+    new ProgressPlugin(),
+    new HTMLWebpackPlugin({ template: paths.html }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+      chunkFilename: 'css/[name].[contenthash].css',
+    }),
+    new DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new HotModuleReplacementPlugin());
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
+    );
+  }
+
+  return plugins;
+};
